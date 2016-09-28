9128755c3388461ce593a8b89457a98af13cdbe0
ver228 <ajaver@MRC-8791.local>
2016-09-28 16:46:48 +0100

    calculate p values

--- work_in_progress/compare_features/read_from_sql_control2.R
+++ work_in_progress/compare_features/read_from_sql_control2.R
@@ -1,4 +1,5 @@
 library(dplyr)
+library(RSQLite)
 library(data.table)
 library(lme4)
 library(lubridate)
@@ -11,17 +12,24 @@ library(doParallel)
 
 RANDOM_EFFECTS = '(1+strain_cmp | plate_id)'
 
-get.model = function(pred_feat, comp_data){
-  fit.full <- lmer(paste0(pred_feat, ' ~ strain_cmp + ', RANDOM_EFFECTS), 
-                  data = comp_data, REML=FALSE, na.action = na.exclude)
+get.model = function(pred_feat, comp_data, frac_thresh = 0.05){
+  feat_dat = comp_data[, pred_feat, with=FALSE]
+  good_frac = 1 - sum(is.na(feat_dat))/dim(comp_data)[1]
+  
+  if(good_frac > frac_thresh)
+    {
+    fit.full <- lmer(paste0(pred_feat, ' ~ strain_cmp + ', RANDOM_EFFECTS), 
+                    data = comp_data, REML=FALSE, na.action = na.exclude)
+  
+    fit.null <- lmer(paste0(pred_feat, ' ~ ', RANDOM_EFFECTS),
+                    data = comp_data, REML=FALSE)
+    
+    likelihood <- anova(fit.null, fit.full, test="F")
+    
+    output = list('likelihood' = likelihood, 'fit.full' = fit.full, 'fit.null' = fit.null)
+    }
+  else {output = NA}
 
-  fit.null <- lmer(paste0(pred_feat, ' ~ ', RANDOM_EFFECTS),
-                  data = comp_data, REML=FALSE)
-  
-  likelihood <- anova(fit.null, fit.full, test="F")
-  
-  output = list('likelihood' = likelihood, 'fit.full' = fit.full, 'fit.null' = fit.null)
-  
   return(output)
 }
 
@@ -32,45 +40,55 @@ progress_wrapper = function(progress_txt, FUNC, ...){
   return(output)
 }
 
-get.mod.parallel = function(pred_feats, comp_data){
-  n_cores <- detectCores() - 1
+get.mod.linear = function(pred_feats, comp_data){
+  get.model.comp = function(feat) {
+    progress_wrapper(feat, get.model, feat, comp_data)
+  }
+  feats_stats = sapply(pred_feats, get.model.comp)
+  return(feats_stats)
+}
+
+get.mod.parallel = function(pred_feats, comp_data, n_cores = detectCores() - 1){
   registerDoParallel(n_cores)
-  feats_stats <- foreach(pred_feat = pred_feats, .combine=list) %dopar%  get.model(feat, comp_data)
+  feats_stats <- foreach(pred_feat = pred_feats, .combine=list) %dopar%  get.model(pred_feat, comp_data)
   stopImplicitCluster()
   return(feats_stats)
 }
 
+get.experiments = function(my_db) { 
+  exp_fields = c("plate_id", "video_id", "base_name", "channel", "experiment_delta", "experiment_timestamp", "strain", "video_timestamp")          
+  exp_sql = sql(paste('SELECT', paste(exp_fields, collapse = ','), 'FROM experiments'))
+  experiments <- as.data.table(tbl(my_db, exp_sql))
+  experiments = experiments[order(plate_id, base_name)]
+  experiments <- within(experiments, {
+    strain <- as.factor(strain)
+    plate_id <- as.factor(plate_id)
+    video_id <- as.factor(video_id)
+    experiment_id <- factor(experiment_timestamp, label=1:length(unique(experiment_timestamp)))
+    experiment_timestamp <- ymd_hms(experiment_timestamp)
+    video_timestamp <- ymd_hms(video_timestamp)
+    day_hour <- hour(video_timestamp) + minute(video_timestamp)/60 + second(video_timestamp)/3600
+  })
+  return(experiments)
+}
 
+get.features.names <- function(my_db){
+  #get list of features to be predicted
+  pred_feats = colnames(tbl(my_db, sql("SELECT * FROM features_means LIMIT 1")))
+  pred_feats = pred_feats[-which(pred_feats %in% c("worm_index", "n_frames", "n_valid_skel", "video_id"))]
+  return(pred_feats)  
+}
 
 
 database_name <- '~/Documents/GitHub/Work_In_Progress/work_in_progress/compare_features/control_experiments.db'
-
 #search the sqlite database
 my_db <- src_sqlite(database_name)
 
-#get list of features to be predicted
-pred_feats = colnames(tbl(my_db, sql("SELECT * FROM features_means LIMIT 1")))
-pred_feats = pred_feats[-which(pred_feats %in% c("worm_index", "n_frames", "n_valid_skel", "video_id"))]
-
-pred_feats = pred_feats #this is for testing i will only explore the first N for speed purposes
 
 
-#get experiments 
-exp_fields = c("plate_id", "video_id", "base_name", "channel", "experiment_delta", "experiment_timestamp", "strain", "video_timestamp")          
-exp_sql = sql(paste('SELECT', paste(exp_fields, collapse = ','), 'FROM experiments'))
-experiments <- as.data.table(tbl(my_db, exp_sql))
-experiments = experiments[order(plate_id, base_name)]
-experiments <- within(experiments, {
-  strain <- as.factor(strain)
-  plate_id <- as.factor(plate_id)
-  video_id <- as.factor(video_id)
-  experiment_id <- factor(experiment_timestamp, label=1:length(unique(experiment_timestamp)))
-  experiment_timestamp <- ymd_hms(experiment_timestamp)
-  video_timestamp <- ymd_hms(video_timestamp)
-  day_hour <- hour(video_timestamp) + minute(video_timestamp)/60 + second(video_timestamp)/3600
-})
 
-#read features data
+#read db data
+experiments = get.experiments(my_db)
 features_means <- as.data.table(tbl(my_db, sql("SELECT * FROM features_means")))
 
 #select only the first video of each experiment (we want to reduce extra data)
@@ -81,14 +99,12 @@ feat_table = as.data.table(merge(exp_tt, features_means, by="video_id"))
 #make strain a key to allow us to search
 setkeyv(feat_table, "strain");
 
-ctr_strain = 'WT' #here the control strain is "WT"
-strains = levels(feat_table$strain)
-strains = strains[-which(strains == ctr_strain)]
 
-#get.model.strain(strain[[1]])
-
-#get.model.strain = function(strain, ctr_strain='WT'){
-  comp_data <- feat_table[c(ctr_strain, strains[[1]])]
+get.model.strain = function(strain, ctr_strain='WT', pred_feats = pred_feats){
+  comp_data <- rbind(feat_table[ctr_strain], feat_table[strain])
+  n_strains_to_compare = length(levels(comp_data$strain))
+  stopifnot(n_strains_to_compare==2)
+  
   comp_data <- within(comp_data, {
     strain_cmp <- strain != ctr_strain
     plate_id <- droplevels(plate_id)
@@ -96,12 +112,25 @@ strains = strains[-which(strains == ctr_strain)]
     experiment_id <- droplevels(experiment_id)
   })
   
-  feats_stats = progress_wrapper('T2', get.mod.parallel, comp_data, pred_feats)
-  
-  #get.model.comp = function(feat) {
-  #  progress_wrapper(feat, get.model, feat, comp_data)
-  #}
-  #feats_stats = progress_wrapper('T1', lapply, pred_feats, get.model.comp)
-  pvalues = sapply(feats_stats, function(x){x$likelihood$`Pr(>Chisq)`[2]})
-#  return(feats_stats)
-#}
+  feats_stats = progress_wrapper('T1', get.mod.linear, pred_feats, comp_data)
+  return(feats_stats)
+}
+
+ctr_strain = 'WT' #here the control strain is "WT"
+strains = levels(feat_table$strain)
+strains = strains[-which(strains == ctr_strain)]
+strains_stats = sapply(strains, get.model.strain)
+names(strains_stats) = strains
+
+read.pvals = function(feats_stats){
+  read.pval = function(x){if(! is.na(x)) x$likelihood$`Pr(>Chisq)`[2] else NA}
+  sapply(feats_stats, read.pval)
+}
+strains_pvals = do.call(rbind, lapply(strains_stats, read.pvals))
+strains_pvals_adj = apply(strains_pvals, 1, function(x) {p.adjust(x, 'BH')})
+
+
+
+
+
+#pvalues = 
