e7100739745c513c7a1eef247101d2b93918f0ed
ver228 <ajaver@MRC-8791.local>
2017-03-17 20:23:17 +0000

    cnn

---
+++ work_in_progress/recognize_worms_nn/untitled2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 17 16:58:09 2017
+
+@author: ajaver
+"""
+import tensorflow as tf
+# Creates a graph.
+with tf.device('/gpu:0'):
+    a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+    b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+    c = tf.matmul(a, b)
+# Creates a session with log_device_placement set to True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# Runs the op.
+print(sess.run(c))
--- work_in_progress/skeletons_CNN/train_tiny.py
+++ work_in_progress/skeletons_CNN/train_tiny.py
@@ -11,72 +11,14 @@ import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten, Reshape, Convolution2D, MaxPooling2D
 from keras.layers.normalization import BatchNormalization
-from keras.utils import np_utils
 from keras.models import load_model
 
-
+import tensorflow as tf
+tf.device('/gpu:0')
 # for reproducibility
 rand_seed = 1337
 np.random.seed(rand_seed)  
 
-
-def test1():
-    out_size = (49, 2)
-    roi_size = 120
-    
-    # number of convolutional filters to use
-    nb_filters = 32
-    # size of pooling area for max pooling
-    pool_size = (2, 2)
-    # convolution kernel size
-    kernel_size = (3, 3)
-    
-    
-    model = Sequential()
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                            border_mode='valid',
-                            input_shape= (roi_size, roi_size, 1)))
-    model.add(Activation('relu'))
-    
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    model.add(Activation('relu'))
-    #model.add(MaxPooling2D(pool_size=pool_size))
-    
-    model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    model.add(Activation('relu'))
-    #model.add(MaxPooling2D(pool_size=pool_size))
-    
-    #model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    #model.add(Activation('relu'))
-    #
-    #model.add(Convolution2D(nb_filters, kernel_size[0], kernel_size[1]))
-    #model.add(Activation('relu'))
-    
-    
-    #model.add(Dropout(0.5))
-    model.add(Flatten())
-    model.add(Dense(2000))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    
-    model.add(Dense(500))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    
-    model.add(Dense(200))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.2))
-    
-    
-    model.add(Dense(out_size[0]*out_size[1]))
-    model.add(Activation('relu'))
-    model.add(Reshape((out_size)))
-    
-    model.compile(loss='mean_absolute_error',
-                  optimizer='adam',
-                  metrics=['mean_absolute_percentage_error'])
-    
-#%%
 out_size = (49, 2)
 roi_size = 120
 
@@ -128,8 +70,6 @@ model.add(Reshape((out_size)))
 model.compile(loss='mean_squared_error',
               optimizer='adam',
               metrics=['mean_absolute_percentage_error'])
-
-
 #%%
 def _get_index(events_tot, val_frac, test_frac):
     inds = np.random.permutation(events_tot)
@@ -144,41 +84,52 @@ def _get_index(events_tot, val_frac, test_frac):
     return all_ind
 
 #%%
-is_debug = False
+is_debug = True
 
 sample_file = 'N2 on food R_2011_03_09__11_58_06___6___3_sampleshort.hdf5'
 with tables.File(sample_file, 'r') as fid:
     
-    X = fid.get_node('/mask')[:][:, :, :, np.newaxis]
-    Y = fid.get_node('/skeleton')[:]
-    
-    X -= 0.5
-    X *= 2.
+    if is_debug:
+        #select a tiny sample
+        tot = fid.get_node('/mask').shape[0]
+        inds = np.random.permutation(tot)[:128]
+        X = fid.get_node('/mask')[inds, :, :][:, :, :, np.newaxis]
+        Y = fid.get_node('/skeleton')[inds, :, :]
+        
+    else:
+        X = fid.get_node('/mask')[:][:, :, :, np.newaxis]
+        Y = fid.get_node('/skeleton')[:]
 
-index = _get_index(X.shape[0], val_frac=0.2, test_frac=0.05)
-X_train, X_val, X_test = [X[index[key]] for key in ['train', 'test', 'val']]
-Y_train, Y_val, Y_test = [Y[index[key]] for key in ['train', 'test', 'val']]
+X -= 0.5
+X *= 2.
+
+#index = _get_index(X.shape[0], val_frac=0.2, test_frac=0.05)
+#X_train, X_val, X_test = [X[index[key]] for key in ['train', 'test', 'val']]
+#Y_train, Y_val, Y_test = [Y[index[key]] for key in ['train', 'test', 'val']]
+#model.fit(X_train, Y_train, batch_size=128, nb_epoch=200,
+#                  verbose=1, validation_data=(X_val, Y_val))
 
 #%%
-nb_epoch = 10
-for i in range(20):
-    model.fit(X_train, Y_train, batch_size=128, nb_epoch=nb_epoch,
-                  verbose=1, validation_data=(X_val, Y_val))
-    model.save('skels_mod2_{}.h5'.format((i+1)*nb_epoch))
+model.fit(X, Y, batch_size=128, nb_epoch=200, verbose=1)
+
+#%%
+#nb_epoch = 10
+#for i in range(20):
+#    model.fit(X_train, Y_train, batch_size=128, nb_epoch=nb_epoch,
+#                  verbose=1, validation_data=(X_val, Y_val))
+#    model.save('skels_mod_{}.h5'.format((i+1)*nb_epoch))
 
 #%%
 
-X, Y = X_test, Y_test
+#X, Y = X_train, Y_train
 
-
-
-model = load_model('skels_mod2_50.h5')
+#model = load_model('skels_mod_40.h5')
 Y_pred = model.predict(X)
 
-#%%
+##%%
 import matplotlib.pylab as plt
 
-ind = 2000
+ind = 0
 plt.figure()
 plt.imshow(np.squeeze(X[ind]), interpolation='None', cmap='gray')
 plt.grid('off')
