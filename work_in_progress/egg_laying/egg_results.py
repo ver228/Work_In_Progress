ff88ab1fd218759ead2d2c0f646afbd5188cf058
ver228 <ver228@gmail.com>
2017-04-03 23:48:14 +0100

    .

--- work_in_progress/egg_laying/egg_results.py
+++ work_in_progress/egg_laying/egg_results.py
@@ -11,8 +11,6 @@ import glob
 import numpy as np
 import matplotlib.pylab as plt
 
-save_results_dir = './results'
-files_done = glob.glob(os.path.join(save_results_dir, '*_eggs.p'))
 
 
 def plot_probs(eggs, worm_probs_resized, worm_probs_fixed, thresh=0.99):
@@ -34,7 +32,7 @@ def plot_probs(eggs, worm_probs_resized, worm_probs_fixed, thresh=0.99):
     
     egg_frames = eggs['frame_number'].values
     
-    plt.figure()
+    #plt.figure()
     plt.subplot(2,1,1)
     _plot_probs(worm_probs_resized, thresh= thresh, maker='o', col='g')
     plt.plot(egg_frames, np.ones(len(egg_frames)), 'xr')
@@ -45,22 +43,27 @@ def plot_probs(eggs, worm_probs_resized, worm_probs_fixed, thresh=0.99):
     plt.plot(egg_frames, np.ones(len(egg_frames)), 'xr')
     plt.title('fixed')
 
+if __name__ == '__main__':
+    save_results_dir = './results'
+    files_done = glob.glob(os.path.join(save_results_dir, '*_eggs.p'))
 
-for ii, fname in enumerate(files_done):
-    print(ii, os.path.basename(fname))
-    
-    results = pickle.load(open(fname, "rb" ))
-    plot_probs(**results)
-    
-    if ii > 10:
-        break
+    for ii, fname in enumerate(files_done):
+        print(ii, os.path.basename(fname))
+        
+        results = pickle.load(open(fname, "rb" ))
+        plot_probs(**results)
+        
+        if ii > 10:
+            break
 
-#%%
-egg_frames = results['eggs']['frame_number'].values
 
-inds_resized = np.where(results['worm_probs_fixed'][:,1]>0.99)[0]
-inds_fixed = np.where(results['worm_probs_resized'][:,1]>0.99)[0]
-#%%
+
+    #%%
+    egg_frames = results['eggs']['frame_number'].values
+
+    inds_resized = np.where(results['worm_probs_fixed'][:,1]>0.99)[0]
+    inds_fixed = np.where(results['worm_probs_resized'][:,1]>0.99)[0]
+    #%%
 
 
 
