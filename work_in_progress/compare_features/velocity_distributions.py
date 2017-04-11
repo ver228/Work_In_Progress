2373f0443ba29cb507536093e7ef379ab3690220
ver228 <ver228@gmail.com>
2017-04-11 09:54:59 +0100

    .

--- work_in_progress/compare_features/velocity_distributions.py
+++ work_in_progress/compare_features/velocity_distributions.py
@@ -8,12 +8,32 @@ Created on Sat Apr  8 11:25:56 2017
 import os
 import glob
 import pandas as pd
+import numpy as np
+import tables
+import matplotlib.pylab as plt
 
+def read_feat_events(feat_file):
+    fid= tables.File(feat_file, 'r')
+    
+    features_events = {}
+    node = fid.get_node('/features_events')
+    for worn_n in node._v_children.keys():
+        worm_node = fid.get_node('/features_events/' + worn_n)
+        
+        for feat in worm_node._v_children.keys():
+            if not feat in features_events:
+                features_events[feat] = {}
+            dat = fid.get_node(worm_node._v_pathname, feat)[:]
+            features_events[feat][worn_n] = dat
+            
+    #features_events = {feat:np.concatenate(val) for feat, val in features_events.items()}
+    return features_events
+#%%
+masks_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/'
+results_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/Results'
 
-#results_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/Results'
-#masks_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/'
-masks_dir = '/Volumes/behavgenom_archive$/Avelino/screening/David_Miller/MaskedVideos/DM_unc-4_Adult_L4_060417'
-results_dir = masks_dir.replace('MaskedVideos', 'Results')
+#masks_dir = '/Volumes/behavgenom_archive$/Avelino/screening/David_Miller/MaskedVideos/DM_unc-4_Adult_L4_060417'
+#results_dir = masks_dir.replace('MaskedVideos', 'Results')
 
 fnames = glob.glob(os.path.join(masks_dir, '*.hdf5'))
 
@@ -23,38 +43,40 @@ for fname in fnames:
     time_str = base_name.rpartition('_')[-1]
     time_rounded = round(int(time_str)/100)
     time_sets[base_name] = time_rounded
-
-
+    
 #%%
-
 feat_files = glob.glob(os.path.join(results_dir, '*_features.hdf5'))
 features_timeseries = {}
+features_events = {}
 for feat_file in feat_files:
     base_name = os.path.basename(feat_file).replace('_features.hdf5', '')
     print(base_name)
     with pd.HDFStore(feat_file, 'r') as fid:
-        features_timeseries[base_name] = fid['/features_timeseries']
+        feat_timeseries = fid['/features_timeseries']
+        feat_timeseries['motion_modes_f'] = np.nan
+        
+        for ind in feat_timeseries['worm_index'].unique():
+            feats_worm = feat_timeseries[feat_timeseries['worm_index']==ind]
+            motion_modes = feats_worm['motion_modes'].fillna(method='ffill')
+            feat_timeseries['motion_modes_f'] = motion_modes
+        
+        features_timeseries[base_name] = feat_timeseries
+        features_events[base_name] = read_feat_events(feat_file)
+
 #%%
 ind_subplot = {key:ii+1 for ii,key in enumerate(sorted(set(time_sets.values())))}
 strain_col = {'unc-4 (c2323)':'b', 'unc-4 (c2323);ceh-12':'r'}
 
-
-import numpy as np
-import matplotlib.pylab as plt
-
 plt.figure()
-for bn, tab in features_timeseries.items():
-    
-    
+for ii, (bn, tab) in enumerate(features_timeseries.items()):
     dat = tab['midbody_speed'].dropna()
     counts, edges = np.histogram(dat, 501, (-25,45))
     
     binsize = edges[1]-edges[0]
     xx = edges[:-1] + (binsize)/2
     
-    yy = counts/dat.size/binsize
-    
-    
+    yy = counts/dat.size#/binsize
+    yy = np.cumsum(yy)
     
     isub = ind_subplot[time_sets[bn]]
     strain = bn.partition('_')[0]
@@ -64,6 +86,8 @@ for bn, tab in features_timeseries.items():
     
     time_str = str(time_sets[bn])
     plt.title(time_str)
+    
+    print(ii, isub, time_sets[bn])
     #ax.set_yscale('log')
     #strain = bn.partition('_')[0]
     #plt.title('{} {}'.format(time_str, strain))
@@ -71,3 +95,33 @@ for bn, tab in features_timeseries.items():
 #%%
 feat_stats = tab.quantile([0.05, 0.5, 0.95])
 feat_stats.append(tab.mean(skipna=True), ignore_index=True)
+#%%
+for ii, (bn, tab) in enumerate(features_timeseries.items()):
+
+    dat = np.concatenate([x for x in features_events[bn]['forward_time'].values()])
+    dat = dat[~np.isnan(dat)]
+    counts, edges = np.histogram(dat, 25)
+    binsize = edges[1]-edges[0]
+    xx = edges[:-1] + (binsize)/2
+    
+    yy = counts/dat.size#/binsize
+    
+    yy = np.cumsum(yy)
+    isub = ind_subplot[time_sets[bn]]
+    strain = bn.partition('_')[0]
+    col = strain_col[strain]
+    ax= plt.subplot(2,2, isub)
+    plt.plot(xx, yy, col)
+    
+    time_str = str(time_sets[bn])
+    plt.title(time_str)
+    
+    print(ii, isub, time_sets[bn])
+
+#%%
+#'forward_distance'
+#'forward_time'
+#'backward_distance'
+#'backward_time'
+#['backward_distance', 'backward_motion_distance_ratio', 'backward_motion_frequency', 'backward_motion_time_ratio', 'backward_time', 'inter_backward_distance', 'inter_backward_time']
+
