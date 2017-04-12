a4b62f52f7da4e67d1fad36e5da0bb1c43dd5739
ver228 <ver228@gmail.com>
2017-04-12 08:29:42 +0100

    .

---
+++ work_in_progress/compare_features/velocity_distributions_v2.py
@@ -0,0 +1,228 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  8 11:25:56 2017
+
+@author: ajaver
+"""
+import os
+import glob
+import pandas as pd
+import numpy as np
+import tables
+import matplotlib.pylab as plt
+from scipy.signal import medfilt
+from tierpsy.analysis.feat_create.obtainFeaturesHelper import WormStats
+
+
+def add_reversal(feat_timeseries, th=-5):
+#    med = feat_timeseries['midbody_speed'].median()
+#    mad = (feat_timeseries['midbody_speed']-med).abs().median()
+#    th = med - 3*mad
+
+    feat_timeseries['is_reversal'] = 0
+    for worm_index, tab in feat_timeseries.groupby('worm_index'):
+        yy = tab['midbody_speed']
+        #yy = tab['length']
+        yy_med = medfilt(yy, 13)
+        
+        feat_timeseries.loc[tab.index, 'is_reversal'] = (yy_med<th)
+    
+    rev_frac = feat_timeseries.groupby('timestamp').agg({'is_reversal':np.mean})
+
+    return rev_frac
+
+
+def read_feat_events(feat_file):
+    fid= tables.File(feat_file, 'r')
+    
+    features_events = {}
+    node = fid.get_node('/features_events')
+    for worn_n in node._v_children.keys():
+        worm_node = fid.get_node('/features_events/' + worn_n)
+        
+        for feat in worm_node._v_children.keys():
+            if not feat in features_events:
+                features_events[feat] = {}
+            dat = fid.get_node(worm_node._v_pathname, feat)[:]
+            features_events[feat][worn_n] = dat
+    
+    def dict2array(dd):
+        return np.concatenate([val for val in dd.values()])
+    
+    features_events = {feat:dict2array(val) for feat, val in features_events.items()}
+    return features_events
+#%%
+masks_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/'
+results_dir = '/Volumes/SAMSUNG_USB/David_Miller/DM_unc-4_Adult_L4_060417/Results'
+
+#masks_dir = '/Volumes/behavgenom_archive$/Avelino/screening/David_Miller/MaskedVideos/DM_unc-4_Adult_L4_060417'
+#results_dir = masks_dir.replace('MaskedVideos', 'Results')
+
+fnames = glob.glob(os.path.join(masks_dir, '*.hdf5'))
+
+
+time_sets = {}
+for fname in fnames:
+    base_name = os.path.basename(fname).replace('.hdf5', '')
+    time_str = base_name.rpartition('_')[-1]
+    time_rounded = round(int(time_str)/100)
+    time_sets[base_name] = time_rounded
+    
+    
+feat_files = glob.glob(os.path.join(results_dir, '*_features.hdf5'))
+for feat_file in  feat_files:
+    with pd.HDFStore(feat_file, 'r') as fid:
+        feat_timeseries = fid['/features_timeseries']
+        
+    rev_frac = add_reversal(feat_timeseries, th=-5)
+    xx = np.arange(rev_frac.size)/25
+    plt.figure()
+    plt.plot(xx, rev_frac)
+    
+    base_name = os.path.basename(feat_file).replace('_features.hdf5', '')
+    plt.title(base_name)
+    plt.xlabel('time (s)')
+    
+    
+#%%
+#feat_files = glob.glob(os.path.join(results_dir, '*_features.hdf5'))
+#features_data = {}
+#wstat = WormStats()
+#plate_stats = np.full(len(feat_files), np.nan, wstat.feat_avg_dtype)
+#for ii, feat_file in enumerate(feat_files):
+#    base_name = os.path.basename(feat_file).replace('_features.hdf5', '')
+#    print(base_name)
+#    with pd.HDFStore(feat_file, 'r') as fid:
+#        feat_timeseries = fid['/features_timeseries']
+#        feat_timeseries['motion_modes_f'] = np.nan
+#        
+#        for ind in feat_timeseries['worm_index'].unique():
+#            feats_worm = feat_timeseries[feat_timeseries['worm_index']==ind]
+#            motion_modes = feats_worm['motion_modes'].fillna(method='ffill')
+#            feat_timeseries['motion_modes_f'] = motion_modes
+#        
+#        dd = {x:feat_timeseries[x] for x in feat_timeseries}
+#        worm_features_dict = {**dd, **read_feat_events(feat_file)}
+#        features_data[base_name] = worm_features_dict
+#        
+#        plate_stats[ii] = wstat.getWormStats(worm_features_dict)
+#plate_stats = pd.DataFrame(plate_stats)
+#
+#plate_stats['base_name'] =[os.path.basename(x).replace('_features.hdf5', '') for x in feat_files]
+#plate_stats['strain'] = [x.partition('_')[0] for x in plate_stats['base_name'].values]
+#plate_stats['time_set'] = [time_sets[x] for x in  plate_stats['base_name'].values]
+##%%
+#
+#feat_timeseries['is_reversal'] = 0
+#for worm_index, tab in feat_timeseries.groupby('worm_index'):
+#    xx = tab['timestamp']
+#    yy = tab['midbody_speed']
+#    #yy = tab['length']
+#    yy_med = medfilt(yy, 13)
+#    
+#    plt.figure()
+#    plt.plot(xx, yy)
+#    plt.plot(xx, yy_med)
+#    #feat_timeseries.loc[tab.index, 'is_reversal'] = (yy_med<th)
+#
+#rev_frac = feat_timeseries.groupby('timestamp').agg({'is_reversal':np.mean})
+#
+#
+
+#%%
+#ind_subplot = {key:ii+1 for ii,key in enumerate(sorted(set(time_sets.values())))}
+#strain_col = {'unc-4 (c2323)':'b', 'unc-4 (c2323);ceh-12':'r'}
+#
+#plt.figure()
+#for ii, (bn, tab) in enumerate(features_data.items()):
+#    dat = tab['midbody_speed'].dropna()
+#    counts, edges = np.histogram(dat, 501, (-25,45))
+#    
+#    binsize = edges[1]-edges[0]
+#    xx = edges[:-1] + (binsize)/2
+#    
+#    yy = counts/dat.size#/binsize
+#    yy = np.cumsum(yy)
+#    
+#    isub = ind_subplot[time_sets[bn]]
+#    strain = bn.partition('_')[0]
+#    col = strain_col[strain]
+#    ax= plt.subplot(2,2, isub)
+#    plt.plot(xx, yy, col)
+#    
+#    time_str = str(time_sets[bn])
+#    plt.title(time_str)
+#    
+#    print(ii, isub, time_sets[bn])
+#    #ax.set_yscale('log')
+#    #strain = bn.partition('_')[0]
+#    #plt.title('{} {}'.format(time_str, strain))
+#
+#%%
+##feat_stats = tab.quantile([0.05, 0.5, 0.95])
+##feat_stats.append(tab.mean(skipna=True), ignore_index=True)
+##%%
+#for ii, (bn, tab) in enumerate(features_data.items()):
+#
+#    
+#    dat = features_data[bn]['forward_time']
+#    dat = dat[~np.isnan(dat)]
+#    counts, edges = np.histogram(dat, 25)
+#    binsize = edges[1]-edges[0]
+#    xx = edges[:-1] + (binsize)/2
+#    
+#    yy = counts/dat.size#/binsize
+#    
+#    #yy = np.cumsum(yy)
+#    isub = ind_subplot[time_sets[bn]]
+#    strain = bn.partition('_')[0]
+#    col = strain_col[strain]
+#    ax= plt.subplot(2,2, isub)
+#    plt.plot(xx, yy, col)
+#    
+#    time_str = str(time_sets[bn])
+#    plt.title(time_str)
+
+#%%
+
+
+
+#%%
+#wstat = WormStats()
+#
+#       feat_stats = np.full(1, np.nan, dtype=self.feat_avg_dtype)
+#
+#        motion_mode = worm_features_dict['motion_modes'].values
+#        for feat_name, feat_props in self.features_info.iterrows():
+#            feat_obj = feat_props['feat_name_obj']
+#
+#            if feat_obj in worm_features._features:
+#                tmp_data = worm_features._features[feat_obj].value
+#            else:
+#                tmp_data = None
+#
+#            if tmp_data is None:
+#                feat_stats[feat_name] = np.nan
+#
+#            elif isinstance(tmp_data, (int, float)):
+#                feat_stats[feat_name] = tmp_data
+#
+#            else:
+#                feat_avg = self._featureStat(
+#                    stat_func,
+#                    tmp_data,
+#                    feat_name,
+#                    feat_props['is_signed'],
+#                    feat_props['is_time_series'],
+#                    motion_mode)
+#                for feat_avg_name in feat_avg:
+#                    feat_stats[feat_avg_name] = feat_avg[feat_avg_name]
+#%%
+
+#'forward_distance'
+#'forward_time'
+#'backward_distance'
+#'backward_time'
+#['backward_distance', 'backward_motion_distance_ratio', 'backward_motion_frequency', 'backward_motion_time_ratio', 'backward_time', 'inter_backward_distance', 'inter_backward_time']
+
--- work_in_progress/optogenetics.py
+++ work_in_progress/optogenetics.py
@@ -5,7 +5,12 @@ Created on Mon Apr 10 14:27:38 2017
 
 @author: ajaver
 """
-
+import glob
+import os
+import tables
+import pandas as pd
+import matplotlib.pylab as plt
+from scipy.signal import medfilt
 import numpy as np
 import cv2
 
@@ -34,9 +39,71 @@ def get_mean_intensity():
     
     video_stats = np.array(video_stats)
     
-    import matplotlib.pylab as plt
     plt.plot(video_stats)
 
 #%%
+def add_reversal(feat_timeseries, th=-5):
+#    med = feat_timeseries['midbody_speed'].median()
+#    mad = (feat_timeseries['midbody_speed']-med).abs().median()
+#    th = med - 3*mad
+
+    feat_timeseries['is_reversal'] = 0
+    for worm_index, tab in feat_timeseries.groupby('worm_index'):
+        yy = tab['midbody_speed']
+        #yy = tab['length']
+        yy_med = medfilt(yy, 13)
+        
+        feat_timeseries.loc[tab.index, 'is_reversal'] = (yy_med<th)
+    
+    rev_frac = feat_timeseries.groupby('timestamp').agg({'is_reversal':np.mean})
+
+    return rev_frac
+
+mask_dir = '/Users/ajaver/OneDrive - Imperial College London/tests/ATR_100417'
+result_dir = mask_dir
+
+feat_files = glob.glob(os.path.join(mask_dir, '*_features.hdf5'))
+mask_files = [x.replace('_features.hdf5', '.hdf5') for x in feat_files]
+
+for mask_file, feat_file in zip(mask_files, feat_files):
+    with tables.File(mask_file) as fid:
+        mean_intensity = fid.get_node('/mean_intensity')[:]
+    
+    with pd.HDFStore(feat_file, 'r') as fid:
+        feat_timeseries = fid['/features_timeseries']
+    
+    med = np.median(mean_intensity)
+    mad = np.median(np.abs(mean_intensity-med))
+    light_on = mean_intensity >  med + mad*6
+    
+    
+    rev_frac = add_reversal(feat_timeseries, th=-5)
+    #%%
+    xx = np.arange(light_on.size)/25
+    
+    plt.figure(figsize=(12,5))
+    d1, = plt.plot(xx, rev_frac, label='fraction of reversals')
+    d2, = plt.plot(xx, light_on, label = 'light on')
+    plt.legend(handles=[d1, d2], loc=2)
+    #ax = sns.lmplot(x='timestamp', y='midbody_speed', hue='worm_index', 
+    #                scatter=True, fit_reg=False, data=feat_timeseries)
+    #plt.plot(-20*light_on)
+    
+    base_name = os.path.basename(mask_file).replace('.hdf5', '')
+    plt.title(base_name)
+    
+    plt.xlabel('time (s)')
+    
+#%%
+
+
+
+    #%%
+
+#    plt.figure()
+#    plt.plot(xx,yy)
+#    plt.plot(xx, yy_med)
+#    plt.plot(xx, yy_med<th)
+
 
 
