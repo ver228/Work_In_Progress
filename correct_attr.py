e7e287422b3bd11b6f78329bf52b72343d9b7249
ver228 <ajaver@MRC-8791.local>
2017-05-04 17:43:35 +0100

    .

--- correct_attr.py
+++ correct_attr.py
@@ -9,11 +9,13 @@ import tables
 import glob
 
 fnames = glob.glob('/Volumes/behavgenom_archive$/Avelino/screening/CeNDR/MaskedVideos/CeNDR_Exp_250417/*.hdf5')
+#fnames = glob.glob('/Volumes/behavgenom_archive$/Avelino/screening/CeNDR/Results/**/*_features.hdf5')
 
 for ii, fname in enumerate(fnames):
     with tables.File(fname, 'r+') as fid:
-        print(ii)
-        print(fid.get_node('/mask').attrs['is_light_background'])
-        fid.get_node('/mask').attrs['is_light_background'] = 1
-        print(fid.get_node('/mask').attrs['is_light_background'])
+        #print(ii, fname)
+        #print(fid.get_node('/features_timeseries').attrs['is_light_background'])
+        print(fid.get_node('/full_data').attrs['is_light_background'])
+        fid.get_node('/full_data').attrs['is_light_background'] = 1
+        print(fid.get_node('/full_data').attrs['is_light_background'])
     
