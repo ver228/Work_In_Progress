94030401d00b7aab02378aaaa5eb4f07c6e5781c
ver228 <ajaver@MRC-8791.local>
2017-07-24 17:06:35 +0100

    .

---
+++ nn_tests/egg_laying/egg_results.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  8 11:11:22 2017
+
+@author: ajaver
+"""
+import os
+import cv2
+import pandas as pd
+import numpy as np
+import matplotlib.pylab as plt
+import glob
+
+save_dir = '/Users/ajaver/OneDrive - Imperial College London/egg_laying/results_N/'
+
+fnames = glob.glob(os.path.join(save_dir, '*.npz'))
+
+fname = fnames[-1]
+data = np.load(fname)
+
+
+Y_pred = data['arr_0']
+Y_true = data['arr_1']
+
+#%%
+plt.figure()
+for kk in range(Y_pred.shape[1]):
+    plt.plot(Y_pred[:,kk,1], '.')
+    plt.plot(Y_true)
+
